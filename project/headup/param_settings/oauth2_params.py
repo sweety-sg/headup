8307b28754bab9e8e3ee0f6ec65c7be6904dc4cc
Sweety <apple@Sweetys-MacBook-Pro.local>
2021-09-05 00:47:48 +0530

    Clean up ignored files

--- project/headup/param_settings/oauth2_params.py
+++
@@ -1,7 +0,0 @@
-
-auth_params = {
-    'CLIENT_ID' : 'ZVHaqsAlP5pA3vvVUJY2X0rd0tZM1h2JUg0qE2s7',
-    'STATE_STRING' : 'random_string',
-    'REDIRECT_URI' : 'http://127.0.0.1:8000/headup/oauth',
-    'CLIENT_SECRET' : 'uUXXl85s5XEYALZh77wTWd0L4hNbKdNZ2a01FkJEiQR0mctCD0bzlkMSWfZ0xRjO8O1TCwYfKMhQCxcp9G5YssSXfEkIPB6zIAyfVlk7tq0ohxqc7RPu5CGKD6Pvmfu2',
-}
